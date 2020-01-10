0b6cbc8b4c3a67ea3687c4619581ad34d3479520
Muahamd Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2020-01-10 16:06:37 +0700

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -12,9 +12,3 @@ while True:
 	print(‘Temp={0:0.1f}*C Humidity={1:0.1f}%’.format(temperature, humidity))
    else:
 	print(‘ Failed to get reading, Try again’)
-
-   # Send the data to Cloud
-   dweepy.dweet_for(‘Humid’, {‘Temp’: temperature})
-   dweepy.dweet_for(‘Humid’, {‘Humid’: humidity})
-
-   # Please check and access dweet.io/follow/Humid
