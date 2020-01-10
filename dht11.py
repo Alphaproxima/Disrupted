58f20aa232952b1863c6c1112ec10b8c2ebe543f
Muahamd Rausyan Fikri <Alphaproxima@users.noreply.github.com>
2020-01-10 16:05:56 +0700

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -1,9 +1,20 @@
-#!/usr/bin/python
-import sys
 import Adafruit_DHT
+import dweepy
+import time
+
+sensor = Adafruit_DHT.DHT11
+gpio = 4
 
 while True:
+   humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+   if humidity is not None and temperature is not None:
+	print(‘Temp={0:0.1f}*C Humidity={1:0.1f}%’.format(temperature, humidity))
+   else:
+	print(‘ Failed to get reading, Try again’)
 
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+   # Send the data to Cloud
+   dweepy.dweet_for(‘Humid’, {‘Temp’: temperature})
+   dweepy.dweet_for(‘Humid’, {‘Humid’: humidity})
+
+   # Please check and access dweet.io/follow/Humid
